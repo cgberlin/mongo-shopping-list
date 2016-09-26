59efa9d4be7328b7bd77e52afa7c75e44217550a
cody <codyberlin@gmail.com>
2016-09-26 09:45:23 -0700

    added mlab db

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            mongodb:cgberlin:Vagrant23@ds041516.mlab.com:41516/shopping-list-cgberlin
 exports.PORT = process.env.PORT || 8080;
